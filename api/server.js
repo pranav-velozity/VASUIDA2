5da665e24be5717501f582ac1f96c7390a267117
pranav-velozity <pranav@velozity.au>
2026-07-29 13:07:17 -0500

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -53,7 +53,7 @@ const corsOptions = {
     return cb(new Error('Not allowed by CORS: ' + origin));
   },
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-pinpoint-client']
 };
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
